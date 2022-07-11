eb7d3bd38dbb8ef955c0080bfa7f209a43381146
ChiobiJason <chiobichisom@gmail.com>
2022-07-11 19:34:05 +0000

    Finished Challenge 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [initialFirstName, setFirstName] = useState("");
+  const [initialLastName, setLastName] = useState("");
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {initialFirstName} {initialLastName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input onChange={handleFirstNameChange} name="fName" placeholder="First Name" />
+        <input onChange={handleLastNameChange} name="lName" placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
